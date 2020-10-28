f76a47c328f0ba313de63e3094a6afec243e1d69
Jesus <kechuu255@hotmail.com>
2020-10-28 09:07:12 -0300

    initial commit

---
+++ PquiaSanAndrsApstol/app/src/main/java/com/kechu/pquiasanandrsapstol/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kechu.pquiasanandrsapstol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextViewRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewRegister = (TextView) findViewById(R.id.textViewRegister);
+
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ PquiaSanAndrsApstol/app/src/main/java/com/kechu/pquiasanandrsapstol/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.kechu.pquiasanandrsapstol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
